ffae88ca9b6d191924d6b7e44b28d54f4645351d
Justtejas <tejaspantoji@gmail.com>
2022-12-25 23:02:55 +0530

    Challenge Completed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # URL Shortener Microservice
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+An API challenge by FreeCodeCamp to develop a URI-Shortener. Completed on 11:02 PM 25 December 2022
--- index.js
+++ index.js
@@ -2,11 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const mongodb = require('mongodb')
+const dns = require('dns');
+const urlparser = require('url');
+const URI = process.env.DB_URI;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('strictQuery', true);
+const URLSchema = new mongoose.Schema({ url: 'string' })
+const URL = mongoose.model('url', URLSchema);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +28,42 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res) => {
+  
+  const originalURL = req.body.url;
+  
+  const parsedLookupUrl = urlparser.parse(originalURL);
+  dns.lookup(parsedLookupUrl.hostname, (err, address) => {
+    if (!address) {
+      res.json({ error: 'invalid url' })
+    } else {
+      const url = new URL({ url: originalURL });
+      url.save((err, data) => {
+        if (err) return console.log(err)
+        res.json({
+          original_url: data.url,
+          short_url: data.id
+        })
+      })
+    }
+  })
+
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const id = req.params.id
+  URL.findById(id, (err, data) => {
+    if (!data) {
+      res.json({
+        error: 'invalid url'
+      })
+    } else {
+      res.redirect(data.url)
+    }
+  })
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
